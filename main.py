8f360995f11b561da3ad7916b23859d226aca643
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-07-02 01:17:50 +0530

    params for conv layer

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from __future__ import absolute_import, division, print_function
 import tensorflow as tf
 import numpy as np
 
-tf.logging.set_verbosity(tf.logging.INFO)
+# tf.logging.set_verbosity(tf.logging.INFO)
 
 def cnn_model_fn(features, labels, mode):
   """Model function for CNN."""
+
+  conv1 = tf.layers.conv2d(
+    inputs=input_layer,
+    filters=32,
+    kernel_size=[5, 5],
+    padding="same",
+    activation=tf.nn.relu)
