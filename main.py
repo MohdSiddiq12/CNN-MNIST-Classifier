5d66e031cd9959611960817f26c1781ae260ced0
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-07-02 20:49:56 +0530

    loading dataset

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ import numpy as np
 
 def cnn_model_fn(features, labels, mode):
   """Model function for CNN."""
+  (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+    # Preprocess the data
+  x_train = x_train / 255.0 
+  x_test = x_test / 255.0
 
   conv1 = tf.layers.conv2d(
     inputs=input_layer,
